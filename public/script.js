d4a97c69a86fab1134f15cb24fdbb38946485429
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-06-16 07:26:11 -0700

    Edited "template" values to "vehicles"

--- public/script.js
+++ public/script.js
@@ -2,6 +2,6 @@ console.log("Script is all hooked up and ready to go, cap'n!");
 
 const header = document.getElementById("HEADER");
 
-fetch("/template").then((response) => response.json()).then((data) => {
+fetch("/vehicles").then((response) => response.json()).then((data) => {
     header.textContent = data;
 });
