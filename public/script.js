02b80399ed63818d935eb7b65fc7dff3511ec9a1
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-06-16 12:53:34 -0700

    added basic styling, built out html elements, built out basic js functionality

--- public/script.js
+++ public/script.js
@@ -1,5 +1,43 @@
 console.log("Script is all hooked up and ready to go, cap'n!");
 
+const exampleObject = {
+    id: 1,
+    name: "Attack Bike",
+    tech_level: 1,
+    weapon_type: "Tiberium Core Missiles",
+    cost: 600,
+    faction: "Brotherhood of Nod (NOD)",
+    stealth: false
+}
+
 const header = document.getElementById("HEADER");
+const entriesArray = document.getElementsByClassName("entry");
+const tableBody = document.getElementById("table").children[0];
+
+
+const fillEntry = (entry, object) => {
+    const items = entry.children;
+    const values = Object.values(object);
+    for (let i = 0; i < items.length; i++) {
+        if (values[i] !== undefined) {
+            items[i].textContent = values[i];
+        }
+    }
+}
+
+const createEntry = (object) => {
+    const tr = document.createElement('tr');
+    for (let i = 0; i < 7; i++) {
+        const td = document.createElement('td');
+        tr.appendChild(td);
+    }
+    tr.children[0].classList += "id";
+    tr.children[2].classList += "integer";
+    tr.children[4].classList += "integer";
+    fillEntry(tr, object);
+    tableBody.appendChild(tr);
+}
+
+createEntry(exampleObject);
 
 fetch("/vehicles").then((response) => response.json()).then((data) => { console.log(data) });
