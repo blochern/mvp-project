21a2370d2eef5c6f4061fb7a8b742f5b91923254
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-06-16 07:32:51 -0700

    .

--- public/script.js
+++ public/script.js
@@ -2,4 +2,4 @@ console.log("Script is all hooked up and ready to go, cap'n!");
 
 const header = document.getElementById("HEADER");
 
-fetch("/vehicles").then((data) => response.json()).then((data) => { console.log(data) });
+fetch("/vehicles").then((response) => response.json()).then((data) => { console.log(data) });
